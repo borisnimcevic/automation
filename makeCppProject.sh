6ebc61d21590620e0100e82a683c5450791a9af8
Boris Nimcevic <borisnimcevic@gmail.com>
2021-04-15 15:46:26 +0200

    Added scripts to auto making cpp project.

--- makeCppProject.sh
+++ makeCppProject.sh
@@ -6,6 +6,7 @@
 mkdir $1
 cd $1
 mkdir build
+mkdir scripts
 touch $1.cpp
 touch CMakeLists.txt
 
@@ -20,4 +21,28 @@ echo "}" >> $1.cpp
 # Make initial cmakefile:
 echo "cmake_minimum_required(VERSION 3.0.0)" >> CMakeLists.txt
 echo "project($1)" >> CMakeLists.txt
+echo "set (CMAKE_RUNTIME_OUTPUT_DIRECTORY \${PROJECT_BINARY_DIR}/bin)" >> CMakeLists.txt
 echo "add_executable(\${PROJECT_NAME} $1.cpp)" >> CMakeLists.txt
+
+# Make scripts
+cd scripts
+touch configure.sh
+echo "#!/bin/zsh" >> configure.sh
+echo "" >> configure.sh
+echo "cmake -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_BUILD_TYPE=Debug -S . -B build/" >> configure.sh
+
+touch build.sh
+echo "#!/bin/zsh" >> build.sh
+echo "" >> build.sh
+echo "cd build" >> build.sh
+echo "make" >> build.sh
+
+touch run.sh
+echo "#!/bin/zsh" >> run.sh
+echo "" >> run.sh
+echo "cd build/bin" >> run.sh
+echo "./$1" >> run.sh
+
+chmod u+x configure.sh
+chmod u+x build.sh
+chmod u+x run.sh
