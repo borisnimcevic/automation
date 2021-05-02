2e34472de28e27de9893e4c308c1bf7d703f7975
Boris Nimcevic <borisnimcevic@gmail.com>
2021-05-02 17:34:02 +0200

    Added a skeleton for component.

--- makeCppProject.sh
+++ makeCppProject.sh
@@ -6,23 +6,39 @@
 mkdir $1
 cd $1
 mkdir build
+mkdir components
 mkdir scripts
 touch $1.cpp
 touch CMakeLists.txt
 
 # Make hellow world code:
 echo "#include <iostream>" >> $1.cpp
+echo "#include \"ExampleComponent.h\"" >> $1.cpp
 echo "" >> $1.cpp
 echo "int main(){" >> $1.cpp
-echo "  std::cout << \"Hello World\" << std::endl;" >> $1.cpp
+echo "  std::cout << \"Hello World \" << add(1, 2) << std::endl;" >> $1.cpp
 echo "  return 0;" >> $1.cpp
 echo "}" >> $1.cpp
 
-# Make initial cmakefile:
+# Make initial CMakeLists.txt:
 echo "cmake_minimum_required(VERSION 3.0.0)" >> CMakeLists.txt
 echo "project($1)" >> CMakeLists.txt
+echo "" >> CMakeLists.txt
+echo "enable_language(C)" >> CMakeLists.txt
+echo "enable_language(CXX)" >> CMakeLists.txt
+echo "" >> CMakeLists.txt
+echo "set (PROJECT_VERSION_MAJOR 1)" >> CMakeLists.txt
+echo "set (PROJECT_VERSION_MINOR 0)" >> CMakeLists.txt
+echo "" >> CMakeLists.txt
 echo "set (CMAKE_RUNTIME_OUTPUT_DIRECTORY \${PROJECT_BINARY_DIR}/bin)" >> CMakeLists.txt
+echo "" >> CMakeLists.txt
 echo "add_executable(\${PROJECT_NAME} $1.cpp)" >> CMakeLists.txt
+echo "" >> CMakeLists.txt
+echo "add_subdirectory(components/ExampleComponent)" >> CMakeLists.txt
+echo "" >> CMakeLists.txt
+echo "target_link_libraries(\${PROJECT_NAME}" >> CMakeLists.txt
+echo "    sub::examplecomponent" >> CMakeLists.txt
+echo ")" >> CMakeLists.txt
 
 # Make scripts
 cd scripts
@@ -46,3 +62,38 @@ echo "./$1" >> run.sh
 chmod u+x configure.sh
 chmod u+x build.sh
 chmod u+x run.sh
+
+# Make an example component:
+cd ../components
+mkdir ExampleComponent
+cd ExampleComponent
+touch ExampleComponent.cpp
+echo "#include \"ExampleComponent.h\"" >> ExampleComponent.cpp
+echo "" >> ExampleComponent.cpp
+echo "int add(int a, int b){" >> ExampleComponent.cpp
+echo "  return a + b;" >> ExampleComponent.cpp
+echo "}" >> ExampleComponent.cpp
+
+touch CMakeLists.txt
+echo "project (ExampleComponent)" >> CMakeLists.txt
+echo "" >> CMakeLists.txt
+echo "add_library(\${PROJECT_NAME}" >> CMakeLists.txt
+echo "  ExampleComponent.cpp" >> CMakeLists.txt
+echo ")" >> CMakeLists.txt
+echo "" >> CMakeLists.txt
+echo "add_library(sub::examplecomponent ALIAS \${PROJECT_NAME})" >> CMakeLists.txt
+echo "" >> CMakeLists.txt
+echo "target_include_directories(\${PROJECT_NAME}" >> CMakeLists.txt
+echo "    PUBLIC " >> CMakeLists.txt
+echo "    \${PROJECT_SOURCE_DIR}/include" >> CMakeLists.txt
+echo ")" >> CMakeLists.txt
+
+mkdir include
+cd include
+touch ExampleComponent.h
+echo "#ifndef _EXAMPLE_COMPONENT_H_" >> ExampleComponent.h
+echo "#define _EXAMPLE_COMPONENT_H_" >> ExampleComponent.h
+echo "" >> ExampleComponent.h
+echo "int add(int, int);" >> ExampleComponent.h
+echo "" >> ExampleComponent.h
+echo "#endif //_EXAMPLE_COMPONENT_H_" >> ExampleComponent.h
