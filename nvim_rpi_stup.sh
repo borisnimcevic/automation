99bf07d6a38a51c79d5674ae34832a46f4833cf2
Boris Nimcevic <borisnimcevic@gmail.com>
2021-12-05 23:47:57 +0100

    WIP: script to setup nvim on raspbian
    Hardware: RaspberyPi 3 model B V1.2

---
+++ nvim_rpi_stup.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+# Boris Nimcevic
+# 2021 December 05
+
+# install prerequisities
+sudo apt-get install -y ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen
+
+# prep to install python3.8
+sudo apt-get install -y build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev
+
+# get and install python3
+wget https://www.python.org/ftp/python/3.8.0/Python-3.8.0.tar.xz
+tar xf Python-3.8.0.tar.xz
+cd Python-3.8.0
+./configure --enable-optimizations --prefix=/usr
+make
+sudo make altinstall
+
+# clean after build
+cd ..
+sudo rm -r Python-3.8.0
+rm Python-3.8.0.tar.xz
+. ~/.bashrc
+
+# make python3.8 a default python
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.8 1
+
+# clone and install
+git clone https://github.com/neovim/neovim
+cd neovim
+rm -r build/  # clear the CMake cache
+make CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/neovim"
+make install
+export PATH="$HOME/neovim/bin:$PATH"
+
+sudo apt install -y clang
+sudo apt install -y ripgrep
+sudo apt install -y perl
+
+# add it as a main editor
+echo "export EDITOR=\"nvim\"" >> $HOME/.bash_profile
+
+# make alias
+echo "alias v=\"nvim\"" >> $HOME/.bashrc
+
+# TODO: check if the dotfiles already exist
+# if not clone from git dotfiles
+# but for now...
+cd
+mkdir .config
+cd .config
+git clone https://github.com/borisnimcevic/nvim.git
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+# PlugInstall, TSUpdate, TSInstall c,cpp,lua
+# Install python
